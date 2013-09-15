59075cd253a6fee559c8a72d320d285ad705b190
Carlos Dalsandos <carlos@Da1sand0s.gency.org>
2013-09-15 08:43:00 -0600

    Working with SSID and Signal Strength + cleanup

--- secops/monitoring/tikimon/wireless_test_rssi.py
+++ secops/monitoring/tikimon/wireless_test_rssi.py
@@ -6,29 +6,11 @@ probeReqs = []
 def sniffProbe(p):
    if p.haslayer(Dot11ProbeReq):
      netName = p.getlayer(Dot11ProbeReq).info
-     #power = p.sprintf("(PrismHeader:%PrismHeader.signal%)")
      rPkt = p.getlayer(RadioTap)
      power_src = -(256-ord(p.notdecoded[-4:-3]))
-     ##power_src = rPkt.notdecoded
+     power_src_str = str(power_src)
 
      if netName not in probeReqs:
        probeReqs.append(netName)
-       print '[+] Detected New Probe Request: ' + netName + power_src
+       print '[+] Detected New Probe Request: ' + netName + ' signal: '  + power_src_str
 sniff(iface=interface, prn=lambda x:sniffProbe(x))
-##sniff(iface=interface, prn=sniffProbe)
-
-
-
-     ##def pktPrint(pkt):
-     ## if pkt.haslayer(Dot11Beacon):
-     ##sniff(iface="mon0", prn=lambda x: x.summary())
-     ##RadioTap / 802.11 Management 8L 00:18:f8:68:c1:bd > ff:ff:ff:ff:ff:ff / Dot11Beacon / SSID='kilozero'
-
-#def sniffRadio(p):
-   #rPkt = p.getlayer(RadioTap)
-   #version = rPkt.version
-   #pad = rPkt.pad
-   #present = rPkt.present
-   #notdecoded=rPkt.notdecoded
-   #nPkt = RadioTap(version=version,pad=pad,present=present,notdecoded=notdecoded)
-   #return nPkt
